287da818fa1bc5630fbc21b43cc296d5f2137709
Yoon Gwan <blackgar@naver.com>
2022-04-19 15:18:03 +0900

    [FE] Feat: Doantion Page UI

--- frontend/src/pages/Routes.tsx
+++ frontend/src/pages/Routes.tsx
@@ -1,12 +1,14 @@
 import { Routes as ReactRouterRoutes, Route, Navigate } from "react-router-dom";
 import Main from "pages/mypage/Main";
 import DonationHistory from "pages/mypage/DonationHistory";
+import Donation from "./donation/Donation";
 
 function Routes() {
   return (
     <ReactRouterRoutes>
       <Route path="/mypage" element={<Main />} />
       <Route path="/donation-history" element={<DonationHistory />} />
+      <Route path="/donation" element={<Donation />} />
     </ReactRouterRoutes>
   );
 }
---
+++ frontend/src/pages/donation/Donation.tsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+function Donation() {
+  return <Container>Donation Page</Container>;
+}
+
+const Container = styled.div``;
+
+const DonationWrapper = styled.div``;
+
+const CreatorWrapper = styled.div``;
+const CreatorName = styled.div``;
+const CreatorContent = styled.div``;
+
+const DonatorWrapper = styled.div``;
+const DonatorName = styled.div``;
+const NameInput = styled.input``;
+const DonatePrice = styled.div``;
+const PriceInput = styled.input``;
+const DonateMessage = styled.div``;
+const MessageTextarea = styled.textarea``;
+
+const TotalWrapper = styled.div``;
+const TotalPrice = styled.div``;
+const TotalUSDC = styled.div``;
+
+const ButtonWrapper = styled.div``;
+const Button = styled.div``;
+export default Donation;
