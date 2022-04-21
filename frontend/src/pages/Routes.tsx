83bf49e5656b15cbec4b904433c92297149c71fb
chosohi <chosohi@naver.com>
2022-04-21 15:46:49 +0900

    [FE] Feat: 지갑 연결 및 서명 기능 개발

--- frontend/src/pages/Routes.tsx
+++ frontend/src/pages/Routes.tsx
@@ -5,10 +5,12 @@ import Donation from "./donation/Donation";
 import NftReward from "pages/mypage/NftReward";
 import ServiceCenter from "./mypage/ServiceCenter";
 import Payment from "./donation/Payment";
+import Home from "./home/Home";
 
 function Routes() {
   return (
     <ReactRouterRoutes>
+      <Route path="/" element={<Home />} />
       <Route path="/mypage" element={<Main />} />
       <Route path="/donation" element={<Donation />} />
       <Route path="/donation-history/*" element={<DonationHistory />} />
