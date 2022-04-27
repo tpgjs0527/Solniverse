86c8a43c2537366e3207d52ba4b27ab41ba6bc61
Yoon Gwan <blackgar@naver.com>
2022-04-28 00:06:57 +0900

    [FE] Feat: Payment in Browser & UI

--- frontend/src/pages/donation/Qrcode.tsx
+++ frontend/src/pages/donation/Qrcode.tsx
@@ -125,61 +125,6 @@ function Qrcode({ open, onClose, params }: IPayment) {
   }, []);
   return (
     <Container>
-      <MobileView>
-        {" "}
-        <Modal
-          isOpen={modalIsOpen}
-          onRequestClose={() => closeModal()}
-          style={mobileStyle}
-        >
-          {/* <Modal open={modalIsOpen} onClose={() => closeModal()} center> */}
-
-          <TitleWrapper style={{ backgroundColor: "#eeeeee", padding: "4px" }}>
-            <PageName>Phantom Wallet 결제</PageName>
-            <OurLogo>
-              <SVGLogo />
-              Solniverse
-            </OurLogo>
-          </TitleWrapper>
-          <Wrapper>
-            <ManualWrapper>
-              <ManualName>Phantom wallet 결제 방법</ManualName>
-              <ManualSeries>
-                <ManualNumber>1️⃣</ManualNumber>
-                <ManualContent>Phantom Wallet 앱 실행</ManualContent>
-              </ManualSeries>
-              <ManualSeries>
-                <ManualNumber>2️⃣</ManualNumber>
-                <ManualContent>
-                  측 상단 QR코드 메뉴 선택 후 오른쪽의 QR코드를 스캔하세요.
-                </ManualContent>
-              </ManualSeries>
-              <ManualSeries>
-                <ManualNumber>3️⃣</ManualNumber>
-                <ManualContent>
-                  이후 표시된 전송 정보를 확인 후 보내기 버튼 클릭
-                </ManualContent>
-              </ManualSeries>
-            </ManualWrapper>
-            <QRWrapper>
-              <QRCodeName>QR코드</QRCodeName>
-              <QRCodeWrapper>
-                <QRCode id="qr-code"></QRCode>
-              </QRCodeWrapper>
-            </QRWrapper>
-          </Wrapper>
-          <Wrapper>
-            <NoWalletGuide>
-              스마트폰으로 쉽고 편리하게 결제할 수 있는 Phantom Wallet 앱을
-              설치하세요!
-            </NoWalletGuide>
-            <WalletInstall>
-              <WalletBtn>설치하기</WalletBtn>
-            </WalletInstall>
-          </Wrapper>
-          <CloseBtn onClick={closeModal}>닫기</CloseBtn>
-        </Modal>
-      </MobileView>
       <BrowserView>
         <Modal
           isOpen={modalIsOpen}
