65fd0afecf13f04e2ce61d9f3cf14a817abe98a0
wonjong <bjlaancgk@naver.com>
2022-05-13 23:03:18 +0900

    [FE] 메인 코드 수정 및 에러페이지 추가

--- frontend/src/pages/my-info/Main.tsx
+++ frontend/src/pages/my-info/Main.tsx
@@ -33,6 +33,19 @@ interface IReceiveResponse {
 function Main() {
   const userInfo = useRecoilValue(userInfoAtom);
 
+  // [BE] 후원한 내역 통계
+  const {
+    isLoading: sendIsLoading,
+    data: sendData,
+    error: sendError,
+  } = useQuery<ISendResponse>(
+    ["send", userInfo.walletAddress],
+    () => fetchSendDashboard(userInfo.walletAddress!)
+    // {
+    //   refetchInterval: 5000,
+    // }
+  );
+
   // [BE] 후원받은 내역 통계
   const {
     isLoading: receiveIsLoading,
@@ -46,87 +59,26 @@ function Main() {
     // }
   );
 
-  // [BE] 후원받은 내역 통계
-  const {
-    isLoading: sendIsLoading,
-    data: sendData,
-    error: sendError,
-  } = useQuery<ISendResponse>(
-    ["send", userInfo.walletAddress],
-    () => fetchSendDashboard(userInfo.walletAddress!)
-    // {
-    //   refetchInterval: 5000,
-    // }
-  );
-
   return (
     <Layout>
       <Title>대시보드</Title>
       <Section>
         <BoxWrapper>
-          {sendData?.ranklist ? (
-            <Dashboard receive={false} data={sendData?.ranklist} />
-          ) : null}
-          {receiveData?.ranklist ? (
-            <Dashboard receive data={receiveData?.ranklist} />
-          ) : null}
+          <Dashboard
+            receive={false}
+            data={sendData?.ranklist}
+            isLoading={sendIsLoading}
+          />
+          <Dashboard
+            receive
+            data={receiveData?.ranklist}
+            isLoading={receiveIsLoading}
+          />
         </BoxWrapper>
       </Section>
     </Layout>
   );
 }
-
-const Icon = styled.div`
-  margin: 14px 0;
-  width: 92px;
-  height: 92px;
-`;
-
-const ColContent = styled.span`
-  letter-spacing: -0.5px;
-  font-weight: 600;
-`;
-
-const ColTitle = styled.span`
-  font-size: 15px;
-  font-weight: 600;
-  letter-spacing: -0.5px;
-  color: ${(props) => props.theme.subTextColor};
-`;
-
-const Col = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const SubBox = styled.div`
-  display: grid;
-  grid-template-columns: repeat(1, 1fr);
-  grid-gap: 50px;
-  padding: 48px 40px;
-
-  @media screen and (min-width: 767px) {
-    grid-template-columns: repeat(4, 1fr);
-    grid-gap: 10px;
-  }
-`;
-
-const SubTitle = styled.p`
-  font-size: 22px;
-  font-weight: 600;
-  letter-spacing: -0.5px;
-  text-align: center;
-`;
-
-const Box = styled.div`
-  min-height: 240px;
-  padding: 25px 24px;
-  border-radius: 14px;
-  box-shadow: 4px 12px 30px 6px rgb(0 0 0 / 9%);
-  background: ${(props) => props.theme.boxColor};
-`;
 
 const BoxWrapper = styled.div`
   display: flex;
