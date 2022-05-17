8b108f5941a4e9fc718e3a4afc1c991a65af392c
Yoon Gwan <blackgar@naver.com>
2022-05-17 16:46:52 +0900

    [FE] Feat:Update Qrcode For USDC

--- frontend/src/pages/donation/Donation.tsx
+++ frontend/src/pages/donation/Donation.tsx
@@ -93,7 +93,7 @@ function Donation() {
       }
     } else {
       navigate({
-        pathname: "/payment",
+        pathname: "/paymentMobile",
         search: `?${createSearchParams(params)}`,
       });
     }
